473219759d6bb0aeae4c7fbdb4bc11dd499983a8
weishunc5 <wilsonchen1993@hotmail.com>
2020-06-04 19:28:41 -0400

    Add files via upload

--- target_of_list.py
+++ target_of_list.py
@@ -13,3 +13,25 @@ def ShowTarget(numlst):
 		target.append(sum)
 
 	return target
+
+def ShowTarget_efficient(numlst):
+	"""
+	start from 40
+	then 40 + 30
+	then 40 + 30 + 20
+	then 40 + 30 + 20 + 10
+	avoid scanning repeatedly
+	"""
+
+	target = []
+	sum = 0
+
+	for i in range(-1, 0 - len(numlst) - 1, -1):
+		sum = sum + numlst[i]
+		target.append(sum)
+
+	result = []
+	for i in range(-1, 0 - len(numlst) - 1, -1): 
+		result.append(target[i])
+
+	return result
